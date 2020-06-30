2800712443189739d91216839447a0807a76726d
정표용 <sur2@naver.com>
2020-06-30 23:40:51 +0900

    가장 큰 수

--- 정렬/가장 큰 수/src/Solution.java
+++ 정렬/가장 큰 수/src/Solution.java
@@ -1,62 +1,53 @@
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-class Element implements Comparable<Element>{
-	String number;
-	
-	Element (String number) {
-		this.number = number;
-	}
-	
-	@Override
-	public int compareTo(Element o) {
-		int oIndex = 0, nIndex = 0;
-		while(oIndex < o.number.length() && number.length() > nIndex) {
-			if (o.number.charAt(oIndex) > number.charAt(nIndex)) {
-				return 1;
-			} else if(o.number.charAt(oIndex) < number.charAt(nIndex)) {
-				return -1;
-			}
-			else {
-				oIndex = (oIndex < o.number.length() - 1) ? oIndex + 1 : oIndex;
-				nIndex = (nIndex < number.length() - 1) ? nIndex + 1 : nIndex;
-				continue;
-			}
-		}
-		return 0;
-	}
-	
-	@Override
-	public String toString() {
-		
-		return number;
-	}
-}
-
 public class Solution {
-	
-	
-	private static int[] numbers = {3, 30, 34, 5, 9};
+
+	private static int[] numbers = {0, 00, 000, 0000, 0, 0, 0};
+
 	public static void main(String[] args) {
 		String answer = "";
-		
-		List<Element> list = new LinkedList<>();
-		
-		for (int i = 0; i < numbers.length; i++) {
-			Element e = new Element(numbers[i] + "");
-			list.add(e);
+		int len = numbers.length;
+		List<int[]> list = new LinkedList<>();
+
+		for (int i = 0; i < len; i++) {
+			int[] temp = new int[2];
+			temp[0] = Number1000(numbers[i]);
+			temp[1] = numbers[i];
+			list.add(temp);
+		}
+
+		list.sort(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o2[0] - o1[0];
+			}
+		});
+
+		for (int[] is : list) {
+			answer += is[1] + "";
 		}
 		
-		Collections.sort(list);
-		System.out.println(list);
-		
-		for (int i = 0; i < list.size(); i++) {
-			answer += list.get(i).number;
+		while(answer.charAt(0) == '0' && answer.length() > 1) {
+			answer = answer.substring(1, answer.length());
 		}
-		
 		System.out.println(answer);
 	}
 
-}
+	private static int Number1000(Integer number) {
+		String temp = number + "";
+		if (number < 10) {
+			temp += number;
+			temp += number;
+			temp += number;
+		} else if (number < 100) {
+			temp += number;
+		} else if (number < 1000) {
+			temp += (temp.charAt(0) + "");
+		}
 
+		return Integer.parseInt(temp);
+	}
+
+}
