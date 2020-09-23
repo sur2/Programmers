35f6ee0a14d1292d49b22c23868fa02179baace3
JEONG PYO YONG <sur2@naver.com>
2020-09-24 00:34:43 +0900

    Level 3 네트워크 → 최적화 Union-Find

--- DFS BFS/네트워크/src/Sol_Union_Find.java
+++ DFS BFS/네트워크/src/Sol_Union_Find.java
@@ -5,6 +5,7 @@ import java.util.Set;
 public class Sol_Union_Find {
 	
 	private int[] root = null;
+	private int[] depth = null;
 
 	public static void main(String[] args) {
 		Sol_Union_Find suf = new Sol_Union_Find();
@@ -17,8 +18,11 @@ public class Sol_Union_Find {
 		int answer = 0;
 		
 		root = new int[n];
+		depth = new int[n];
+		
 		for (int i = 0; i < n; i++) {
 			root[i] = i;
+			depth[i] = 1;
 		}
 		System.out.println(Arrays.toString(root));
 		
@@ -33,6 +37,7 @@ public class Sol_Union_Find {
 		Set<Integer> set = new HashSet<Integer>();
 		for (int i = 0; i < n; i++) {
 			set.add(Find(i));
+			//set.add(root[i]);
 		}
 		
 		answer = set.size();
@@ -47,9 +52,23 @@ public class Sol_Union_Find {
 		x = Find(x);
 		y = Find(y);
 		
-		// 찾아다면 x를 기준으로 y 노드를 추가하자!
-		// y의 root 노드는 x이다!
-		root[y] = x;
+		// 두 노드의 root 노드가 같으면 같은 트리로 보고 합치지 않음!
+		if (x == y) {
+			return;
+		}
+		
+		// Union 최적화
+		// 항상 더 높은(깊은) 트리 밑으로 낮은 트리를 넣어서 합친다!
+		if (depth[x] > depth[y]) {
+			// 찾아다면 x를 기준으로 y 노드를 추가하자!
+			// y의 root 노드는 x이다!
+			root[y] = x;
+			depth[x] += depth[y];
+		}
+		else {
+			root[x] = y;
+			depth[y] += depth[x];
+		}
 	}
 	
 	// n의 root 노드를 찾자!
@@ -59,7 +78,9 @@ public class Sol_Union_Find {
 			return n;
 		}
 		else {
-			return Find(root[n]);
+			// 경로 압축
+			// Find 함수가 호출 되면서 만나는 부모 노드를 root 노드로 갱신한다.
+			return root[n] = Find(root[n]);
 		}
 	}
 
