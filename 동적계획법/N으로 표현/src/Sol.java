c4fe5a9bb7ae22bb38421bc67fa05c6a9ac7c4ac
JEONG PYO YONG <sur2@naver.com>
2020-09-27 02:36:33 +0900

    Level 3 N으로 표현

--- 동적계획법/N으로 표현/src/Sol.java
+++ 동적계획법/N으로 표현/src/Sol.java
@@ -12,7 +12,7 @@ public class Sol {
 	
 	public int solution(int N, int number) {
         int answer = 0;
-        
+        answer = -1;
         // Set[8] 배열 선언 및 초기화
         Set<Integer> set[] =  new HashSet[8];
         int n = 0;
@@ -23,19 +23,31 @@ public class Sol {
 			set[i].add(n);
 		}
         
-        for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				for (int ii = 0; ii < set[i].size(); ii++) {
-					for (int jj = 0; jj < set[j].size(); jj++) {
-						
+        // 최솟값 8만큼 사용된 숫자 별로 연산가능한 경우의 수 Set에 저장
+        for (int i = 1; i < 8; i++) { 
+			for (int j = 0; j < i; j++) {
+				for (Integer k : set[j]) {
+					for (Integer l : set[i - 1 - j]) {
+						if (k + l < 32001)
+							set[i].add(k + l);
+						if (k - l > 0)
+							set[i].add(k - l);
+						if (k * l < 32001)
+							set[i].add(k * l);
+						if (l != 0) 
+							set[i].add(k / l);
 					}
 				}
 			}
 		}
-
         
-        
-        
+        for (int i = 0; i < 8; i++) {
+        	if(set[i].contains(number)) {
+        		answer = i + 1;
+        		break;
+        	}
+		}
+        System.out.println(answer);
         return answer;
     }
 
